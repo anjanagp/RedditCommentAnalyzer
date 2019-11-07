9461cecdec80977ea8a3c63fd4c90a533d33c7e8
Anjana Guruprasad <anjana.gp@gmail.com>
2019-11-07 00:27:59 -0800

    Improve pre-processing and organizing into Classes

--- analyze_reddit_comments.py
+++ analyze_reddit_comments.py
@@ -3,15 +3,16 @@
 # Part 2 : Create an Inverted Index to develop a Boolean Retrieval Model
 # Notes
 #       : Add error handling for invalid Command Line arg
-#       : Create a class (organize code)
 #       : Add a function for intersection of sets to handle boolean queries
-#       : Better pre-processing data, like tokenizing, etc
+#       : Use NoSQL MongoDB to store inverted index
 #------------------------------------------------------------------------------#
 import os
 import sys
 import praw
 from nltk.corpus import stopwords
-
+from nltk.tokenize import word_tokenize
+from nltk.stem import PorterStemmer
+porter= PorterStemmer()
 stop_words = set(stopwords.words('english'))
 
 # Create reddit object using praw
@@ -19,36 +20,102 @@ reddit = praw.Reddit(client_id=os.getenv('CLIENT_ID'),
                     client_secret=os.getenv('CLIENT_SECRET'),
                     user_agent='subSentiment')
 
-inverted_index = {} # where key is the term and value is the postings list
+class Fetcher:
+    def __init__(self, reddit_object):
+        self.reddit = reddit_object
+        self.submission_ids = []
+        self.comment_id_storage = {}
 
-def collect_top_comments(topic, no_of_submissions=10):
-    """
-    This function collects the top comments for the given topic(subreddit) and
-    saves it into a text file.
-    Args:
-        topic [string] : name of subreddit (for example for /r/aww pass 'aww')
-        no_of_submissions [integer] : limit on number of submissions to crawl
-    Returns:
-        inverted_index [dict] : a dictionary where each key is a term and the
-        corresponsing value is its postings list
-    """
-    subreddit = reddit.subreddit(topic)
-    no_of_comments_collected = 0
-    with open('topComments.txt', 'w') as f:
+    def get_top_submission_ids(self, sub, no_of_submissions=10):
+        """
+        This function collects the top submissions for the given subreddit
+        Args:
+            sub [string] : name of subreddit (for example for /r/aww pass 'aww')
+            no_of_submissions [integer] : limit on number of submissions to crawl
+        Returns:
+            list of submission ids
+        """
+        subreddit = reddit.subreddit(sub)
         for submission in subreddit.hot(limit=no_of_submissions):
-            submission.comments.replace_more(limit=0)
-            top_level_comments = list(submission.comments)
-            for comment in top_level_comments:
-                terms = (comment.body).split() # list of terms
-                for t in terms:
-                    if t not in stop_words:
-                        inverted_index.setdefault(t, [])
-                        if submission.id not in inverted_index[t]:
-                            inverted_index[t].append(submission.id)
-                f.write(comment.body)
-                no_of_comments_collected = no_of_comments_collected + 1
-    return inverted_index
+            self.submission_ids.append(submission.id)
+        return self.submission_ids
 
-index = collect_top_comments(topic= sys.argv[1], no_of_submissions=10)
-inverted_index.update({k: sorted(v) for k, v in inverted_index.items()}) # sorting postings list in ascending order of submission ID
-print("inverted index is ", inverted_index)
+    def get_submission_comment_ids(self, submission_id):
+        """
+        Fetches all the top level comments for the given submission id
+        Also populates comment_id_storage
+        Args:
+            submission_id
+        Returns:
+            list of comment ids
+        """
+        submission = reddit.submission(id=submission_id)
+        submission.comments.replace_more(limit=0)
+        top_comment_ids = list(submission.comments)
+        self.comment_id_storage[submission_id] = top_comment_ids
+        return top_comment_ids
+
+class PreprocessData:
+
+    def remove_stop_words(self, comment_id):
+        """
+        Removes all stop words from data and tokenizes it
+        Returns a list of preprocessed terms derived from the comment
+        Args:
+            comment_id
+        """
+        preprocessed_comment = []
+        tokens = word_tokenize(comment_id.body)
+        words = [word for word in tokens if word.isalpha()]
+        for w in words:
+            if not w in stop_words:
+                preprocessed_comment.append(porter.stem(w))
+        return preprocessed_comment
+
+class Inverted_Index:
+    def __init__(self):
+        self.index = dict()
+
+    def index_comment(self, submission_id, comment_list):
+        """
+        Indexes a comment list by populating the index where they key is the
+        submission_id and the value is a postings list which contains all the
+        terms that belong to that submission_id
+        Args:
+            submission_id
+            comment_list
+        """
+        for comment in comment_list:
+            for term in comment:
+                self.index.setdefault(term, [])
+                if submission_id not in self.index[term]:
+                    self.index[term].append(submission_id)
+        return self.index
+
+    def lookup_query(self, query):
+        """
+        """
+        # take a boolean query and return the list of correct submission ID
+        print("Not implemented")
+
+# Fetch data
+fetcher_object = Fetcher(reddit)
+list_of_ids = fetcher_object.get_top_submission_ids(sub= sys.argv[1], no_of_submissions=1)
+for i in list_of_ids:
+    fetcher_object.get_submission_comment_ids(i)
+storage_object = fetcher_object.comment_id_storage
+preprocess_object = PreprocessData()
+# Preprocess data
+for k, v in storage_object.items():
+    temp = []
+    for comment_id in v:
+        temp.append(preprocess_object.remove_stop_words(comment_id))
+    storage_object[k] = temp
+# Create Inverted Index
+final_index = Inverted_Index()
+for k,v in storage_object.items():
+    final_index.index_comment(k, v)
+print (final_index.index)
+
+final_index.index.update({k: sorted(v) for k, v in final_index.index.items()}) # sorting postings list in ascending order of submission ID
+# print("inverted index is ", final_index.index)
